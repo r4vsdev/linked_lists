edd3f720a72bb7922bb2ecc51e41009d0a02ba38
r4vsdev <r4vsdev@gmail.com>
2023-03-30 00:11:16 -0300

    Set nil as default to value and next node

--- linked_lists.rb
+++ linked_lists.rb
@@ -8,14 +8,16 @@ class LinkedList
 end
 
 class Node
-  def initialize(data, next)
-
+  def initialize(*data)
+    data == [] ? @data = nil : @data = data
+    @next = nil
   end
 
   def value
-    
+    @data
   end
-
 end
 
-p Node.new("John", 46)
+# p node = Node.new("John", 46)
+p node = Node.new()
+p node.value
