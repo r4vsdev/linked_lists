738183f270e675ec18db3966709728c0ad71ca7e
r4vsdev <r4vsdev@gmail.com>
2023-03-31 17:32:15 -0300

    Add #append

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,23 +1,35 @@
-class LinkedList
+# frozen_string_literal: true
 
+# creates a Linked List
+class LinkedList
+  def initialize
+    @head = nil
+  end
 
   def append(value)
-
-  end
-
-end
-
-class Node
-  def initialize(*data)
-    data == [] ? @data = nil : @data = data
-    @next = nil
-  end
-
-  def value
-    @data
+    puts "appending #{value}"
+    new_node = Node.new(value)
+    if @head == nil
+      @head = new_node
+    else
+      @head.pointer = new_node
+    end
   end
 end
 
-# p node = Node.new("John", 46)
-p node = Node.new()
-p node.value
+# creates a node with default value and pointer set to nil
+class Node < LinkedList
+  attr_accessor :pointer, :value
+
+  def initialize(value = nil, pointer = nil)
+    @value = value
+    @pointer = pointer
+  end
+end
+
+list = LinkedList.new
+p list
+list.append('Pedro')
+p list
+list.append(3)
+p list
